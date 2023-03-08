feeb44da3ddc4ef8fc9c9b6cffd3058272d03c1f
zachloh <leongaun17@gmail.com>
2023-03-08 21:25:06 +1100

    Add query for fetching products by category

---
+++ src/api/products/getProductsByCategory.ts
@@ -0,0 +1,9 @@
+import { customAxios } from '@/lib/axios';
+import { Product, StrapiResponse } from '@/types';
+
+export const getProductsByCategory = async (category: string) => {
+  const { data } = await customAxios.get<StrapiResponse<Product>>(
+    `/products?populate=*&filters[category][title][$eq]=${category}`
+  );
+  return data;
+};
