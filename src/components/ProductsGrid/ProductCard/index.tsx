0d70d1ac07ae72d03e916d363a3e9d47e501970d
zachloh <leongaun17@gmail.com>
2023-03-17 16:48:05 +1100

    Allow users to add to wishlist in ProductCard

--- src/components/ProductsGrid/ProductCard/index.tsx
+++ src/components/ProductsGrid/ProductCard/index.tsx
@@ -1,7 +1,10 @@
+import { useWishlist, useWishlistStore } from '@/stores/wishlist';
 import { Product } from '@/types';
-import { Text } from '@mantine/core';
+import { Anchor, Text, UnstyledButton } from '@mantine/core';
 import Image from 'next/image';
-import React from 'react';
+import Link from 'next/link';
+import React, { useEffect, useState } from 'react';
+import { FiHeart } from 'react-icons/fi';
 import styles from './ProductCard.module.css';
 
 type ProductCardProps = {
@@ -9,45 +12,89 @@ type ProductCardProps = {
 };
 
 const ProductCard = ({ product }: ProductCardProps) => {
+  const { wishlist } = useWishlist();
+  const toggleWishlist = useWishlistStore((state) => state.toggleWishlist);
+  const [clicked, setClicked] = useState(false);
+
+  useEffect(() => {
+    const isAddedToWishlist = wishlist.some((item) => item.id === product.id);
+    if (isAddedToWishlist) {
+      setClicked(true);
+    }
+  }, [wishlist, product.id]);
+
+  const handleClickWishlist = () => {
+    toggleWishlist(product);
+    setClicked((c) => !c);
+  };
+
   return (
-    <figure>
-      <div className={styles.wrapper}>
-        <Image
-          src={product.attributes.image1.data.attributes.url}
-          alt={product.attributes.image1.data.attributes.alternativeText}
-          fill
-          style={{ objectFit: 'cover' }}
-          // TODO: Set sizes
-          sizes="25vw"
-          placeholder="blur"
-          blurDataURL={product.attributes.image1.data.attributes.placeholder}
-          className={styles.primary}
-        />
-        <Image
-          src={product.attributes.image2.data.attributes.url}
-          alt={product.attributes.image2.data.attributes.alternativeText}
-          fill
-          style={{ objectFit: 'cover' }}
-          // TODO: Set sizes
-          sizes="25vw"
-          placeholder="blur"
-          blurDataURL={product.attributes.image2.data.attributes.placeholder}
-          className={styles.secondary}
-        />
-      </div>
-      <Text size={14} weight={600} mt={10}>
-        {product.attributes.brand.data.attributes.title}
-      </Text>
-      <Text size={14}>{product.attributes.title}</Text>
-      <Text size={14} weight={600} my={3}>
-        ${product.attributes.price}
-      </Text>
-      {product.attributes.isNew && (
-        <Text size={12} weight={600} color="cyan.9">
-          NEW
+    <>
+      <figure className={styles.figure}>
+        <Anchor
+          component={Link}
+          href={`/product/${product.id}`}
+          className={styles.link}
+        >
+          <div className={styles.wrapper}>
+            <Image
+              src={product.attributes.image1.data.attributes.url}
+              alt={product.attributes.image1.data.attributes.alternativeText}
+              fill
+              style={{ objectFit: 'cover', objectPosition: '50% 10%' }}
+              // TODO: Set sizes
+              sizes="25vw"
+              placeholder="blur"
+              blurDataURL={
+                product.attributes.image1.data.attributes.placeholder
+              }
+              className={styles.primary}
+            />
+            <Image
+              src={product.attributes.image2.data.attributes.url}
+              alt={product.attributes.image2.data.attributes.alternativeText}
+              fill
+              style={{ objectFit: 'cover', objectPosition: '50% 10%' }}
+              // TODO: Set sizes
+              sizes="25vw"
+              placeholder="blur"
+              blurDataURL={
+                product.attributes.image2.data.attributes.placeholder
+              }
+              className={styles.secondary}
+            />
+          </div>
+        </Anchor>
+        <UnstyledButton
+          className={styles['heart-button']}
+          onClick={handleClickWishlist}
+          aria-label="Add to wishlist"
+        >
+          <FiHeart
+            className={`${styles.heart} ${clicked ? styles.active : ''}`}
+          />
+        </UnstyledButton>
+      </figure>
+      <Anchor
+        component={Link}
+        href={`/product/${product.id}`}
+        color="dark"
+        className={styles.link}
+      >
+        <Text size={14} weight={600} mt={10}>
+          {product.attributes.title}
         </Text>
-      )}
-    </figure>
+        <Text size={14}>{product.attributes.brand.data.attributes.title}</Text>
+        <Text size={14} weight={600} my={3}>
+          ${product.attributes.price}.00
+        </Text>
+        {product.attributes.isNew && (
+          <Text size={12} weight={600} color="cyan.9">
+            NEW
+          </Text>
+        )}
+      </Anchor>
+    </>
   );
 };
 
