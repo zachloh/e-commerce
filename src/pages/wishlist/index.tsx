37470fbb53891774ad331c3a98a0f3463f4084e5
zachloh <leongaun17@gmail.com>
2023-03-21 01:12:18 +1100

    Make wishlist page responsive

--- src/pages/wishlist/index.tsx
+++ src/pages/wishlist/index.tsx
@@ -11,13 +11,24 @@ const Wishlist = () => {
     return null;
   }
 
+  // TODO: Show when no wishlist item
+
   // TODO: Animate removing item
   return (
     <Container size={1400} p={16} pt={30} pb={50}>
       <Title order={1} size={28} mb={20}>
         My Wishlist
       </Title>
-      <SimpleGrid cols={4} spacing={48}>
+      <SimpleGrid
+        cols={1}
+        spacing={36}
+        breakpoints={[
+          { minWidth: 500, cols: 2, spacing: 24 },
+          { minWidth: 768, cols: 3, spacing: 24 },
+          { minWidth: 1200, cols: 4, spacing: 24 },
+          { minWidth: 1400, cols: 4, spacing: 48 },
+        ]}
+      >
         {wishlist.map((item) => (
           <div key={item.id}>
             <WishlistItem item={item} />
