ceff72d9d5d2cdadfede7bbbd0e1403f15c35ad4
zachloh <leongaun17@gmail.com>
2023-03-24 15:15:56 +1100

    Add animation on removing a wishlist/cart item

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,7 +1,8 @@
 import CartItem from '@/components/CartItem';
 import OrderSummary from '@/components/OrderSummary';
 import { useCart } from '@/stores/cart';
-import { Container, Grid, Title } from '@mantine/core';
+import { Container, Grid, MediaQuery, rem, Title } from '@mantine/core';
+import { motion, AnimatePresence } from 'framer-motion';
 import React from 'react';
 
 const Cart = () => {
@@ -9,31 +10,51 @@ const Cart = () => {
 
   const { cart, isHydrated } = useCart();
 
-  // TODO: Show skeleton
   if (!isHydrated) {
     return null;
   }
 
-  // TODO: Add animation for when cart item is removed
-  // TODO: Add scrollarea or set order summary position to fixed
   return (
-    <Container size={1400} p={16} pt={30} pb={50}>
-      <Title order={1} size={28} mb={20}>
-        My Cart
-      </Title>
-      <Grid m={0} gutterSm={30} gutterLg={50}>
-        <Grid.Col span={12} sm={6} md={7} p={0}>
-          {cart.map((item) => (
-            <div key={`${item.id}-${item.size}`}>
-              <CartItem item={item} />
-            </div>
-          ))}
-        </Grid.Col>
-        <Grid.Col span={12} sm={6} md={5} p={0}>
-          <OrderSummary />
-        </Grid.Col>
-      </Grid>
-    </Container>
+    <MediaQuery
+      largerThan={768}
+      styles={{ paddingLeft: rem(32), paddingRight: rem(32) }}
+    >
+      <Container size={1400} p={16} pt={30} pb={50}>
+        <Title order={1} size={28}>
+          My Cart
+        </Title>
+        <Grid m={0} gutterSm={30} gutterLg={50} align="flex-start">
+          <Grid.Col span={12} sm={6} md={7} p={0} mt={15}>
+            <AnimatePresence>
+              {cart.map((item) => (
+                <motion.div
+                  key={`${item.id}-${item.size}`}
+                  layout
+                  initial={false}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0 }}
+                  transition={{
+                    opacity: { ease: 'linear' },
+                    layout: { duration: 0.4 },
+                  }}
+                >
+                  <CartItem item={item} />
+                </motion.div>
+              ))}
+            </AnimatePresence>
+          </Grid.Col>
+          <Grid.Col
+            span={12}
+            sm={6}
+            md={5}
+            p={0}
+            sx={{ position: 'sticky', top: '176px' }}
+          >
+            <OrderSummary />
+          </Grid.Col>
+        </Grid>
+      </Container>
+    </MediaQuery>
   );
 };
 
--- src/pages/wishlist/index.tsx
+++ src/pages/wishlist/index.tsx
@@ -1,41 +1,57 @@
 import WishlistItem from '@/components/WishlistItem';
 import { useWishlist } from '@/stores/wishlist';
-import { Container, SimpleGrid, Title } from '@mantine/core';
+import { Container, MediaQuery, rem, SimpleGrid, Title } from '@mantine/core';
+import { motion, AnimatePresence } from 'framer-motion';
 import React from 'react';
 
 const Wishlist = () => {
   const { wishlist, isHydrated } = useWishlist();
 
-  // TODO: Show skeleton
   if (!isHydrated) {
     return null;
   }
 
   // TODO: Show when no wishlist item
 
-  // TODO: Animate removing item
   return (
-    <Container size={1400} p={16} pt={30} pb={50}>
-      <Title order={1} size={28} mb={20}>
-        My Wishlist
-      </Title>
-      <SimpleGrid
-        cols={1}
-        spacing={36}
-        breakpoints={[
-          { minWidth: 500, cols: 2, spacing: 24 },
-          { minWidth: 768, cols: 3, spacing: 24 },
-          { minWidth: 1200, cols: 4, spacing: 24 },
-          { minWidth: 1400, cols: 4, spacing: 48 },
-        ]}
-      >
-        {wishlist.map((item) => (
-          <div key={item.id}>
-            <WishlistItem item={item} />
-          </div>
-        ))}
-      </SimpleGrid>
-    </Container>
+    <MediaQuery
+      largerThan={768}
+      styles={{ paddingLeft: rem(32), paddingRight: rem(32) }}
+    >
+      <Container size={1400} p={16} pt={30} pb={50}>
+        <Title order={1} size={28} mb={20}>
+          My Wishlist
+        </Title>
+        <SimpleGrid
+          cols={1}
+          spacing={36}
+          breakpoints={[
+            { minWidth: 500, cols: 2, spacing: 24 },
+            { minWidth: 768, cols: 3, spacing: 24 },
+            { minWidth: 1200, cols: 4, spacing: 24 },
+            { minWidth: 1400, cols: 4, spacing: 48 },
+          ]}
+        >
+          <AnimatePresence>
+            {wishlist.map((item) => (
+              <motion.div
+                key={item.id}
+                layout
+                initial={false}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{
+                  opacity: { ease: 'linear' },
+                  layout: { duration: 0.4 },
+                }}
+              >
+                <WishlistItem item={item} />
+              </motion.div>
+            ))}
+          </AnimatePresence>
+        </SimpleGrid>
+      </Container>
+    </MediaQuery>
   );
 };
 
