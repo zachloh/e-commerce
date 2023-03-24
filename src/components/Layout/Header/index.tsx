7b611dbd28329cf4c42cd620f5101e7ee6bb61f7
zachloh <leongaun17@gmail.com>
2023-03-24 15:19:35 +1100

    Remove search icon from header

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -8,11 +8,12 @@ import {
   Group,
   Indicator,
   MediaQuery,
+  rem,
 } from '@mantine/core';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import React from 'react';
-import { Heart, ShoppingCart, Search } from 'tabler-icons-react';
+import { Heart, ShoppingCart } from 'tabler-icons-react';
 import styles from './Header.module.css';
 
 type HeaderProps = {
@@ -28,75 +29,69 @@ const Header = ({ opened, onToggleNavbar, onClose }: HeaderProps) => {
   const { wishlist } = useWishlist();
 
   return (
-    <Grid align="center" mx="auto" h="100%" m={0} px={16} maw={1400}>
-      <Grid.Col span="auto" p={0}>
-        <MediaQuery smallerThan={768} styles={{ display: 'none' }}>
-          <Group spacing={30}>
-            <Anchor
-              component={Link}
-              href="/category/men"
-              color="white"
-              weight={600}
-              className={`${styles.link} ${
-                currentRoute === '/category/men' ? styles.active : ''
-              }`}
-            >
-              MEN
-            </Anchor>
-            <Anchor
-              component={Link}
-              href="/category/women"
-              color="white"
-              weight={600}
-              className={`${styles.link} ${
-                currentRoute === '/category/women' ? styles.active : ''
-              }`}
-            >
-              WOMEN
-            </Anchor>
-          </Group>
-        </MediaQuery>
-        <MediaQuery largerThan={768} styles={{ display: 'none' }}>
-          <Burger
-            opened={opened}
-            onClick={onToggleNavbar}
-            size="sm"
-            color="white"
-            aria-label="Open navigation"
-          />
-        </MediaQuery>
-      </Grid.Col>
-      <Grid.Col span="content" p={0}>
-        <Anchor
-          component={Link}
-          href="/"
-          color="white"
-          weight={650}
-          className={styles.logo}
-        >
-          LUXE
-        </Anchor>
-      </Grid.Col>
-      <Grid.Col span="auto" p={0}>
-        <Group position="right" spacing={16}>
-          <MediaQuery smallerThan={600} styles={{ display: 'none' }}>
-            <ActionIcon
-              component={Link}
-              href="/"
-              variant="transparent"
-              aria-label="Search"
-              className={styles.icon}
-            >
-              <Search color="white" />
-            </ActionIcon>
+    <MediaQuery
+      largerThan={768}
+      styles={{ paddingLeft: rem(32), paddingRight: rem(32) }}
+    >
+      <Grid align="center" mx="auto" h="100%" m={0} px={16} maw={1400}>
+        <Grid.Col span="auto" p={0}>
+          <MediaQuery smallerThan={768} styles={{ display: 'none' }}>
+            <Group spacing={30}>
+              <Anchor
+                component={Link}
+                href="/category/men"
+                color="white"
+                weight={600}
+                className={`${styles.link} ${
+                  currentRoute === '/category/men' ? styles.active : ''
+                }`}
+              >
+                MEN
+              </Anchor>
+              <Anchor
+                component={Link}
+                href="/category/women"
+                color="white"
+                weight={600}
+                className={`${styles.link} ${
+                  currentRoute === '/category/women' ? styles.active : ''
+                }`}
+              >
+                WOMEN
+              </Anchor>
+            </Group>
           </MediaQuery>
-          <MediaQuery smallerThan={600} styles={{ display: 'none' }}>
+          <MediaQuery largerThan={768} styles={{ display: 'none' }}>
+            <Burger
+              opened={opened}
+              onClick={onToggleNavbar}
+              size="sm"
+              color="white"
+              aria-label="Open navigation"
+            />
+          </MediaQuery>
+        </Grid.Col>
+        <Grid.Col span="content" p={0}>
+          <Anchor
+            component={Link}
+            href="/"
+            color="white"
+            weight={650}
+            className={styles.logo}
+            onClick={onClose}
+          >
+            LUXE
+          </Anchor>
+        </Grid.Col>
+        <Grid.Col span="auto" p={0}>
+          <Group position="right" className={styles['icons-wrapper']}>
             <ActionIcon
               component={Link}
               href="/wishlist"
               variant="transparent"
               aria-label="Wishlist"
               className={styles.icon}
+              onClick={onClose}
             >
               <Indicator
                 size={16}
@@ -107,26 +102,27 @@ const Header = ({ opened, onToggleNavbar, onClose }: HeaderProps) => {
                 <Heart color="white" />
               </Indicator>
             </ActionIcon>
-          </MediaQuery>
-          <ActionIcon
-            component={Link}
-            href="/cart"
-            variant="transparent"
-            aria-label="Cart"
-            className={styles.icon}
-          >
-            <Indicator
-              size={16}
-              label={totalQuantity}
-              disabled={totalQuantity === 0}
-              color="cyan.9"
+            <ActionIcon
+              component={Link}
+              href="/cart"
+              variant="transparent"
+              aria-label="Cart"
+              className={styles.icon}
+              onClick={onClose}
             >
-              <ShoppingCart color="white" />
-            </Indicator>
-          </ActionIcon>
-        </Group>
-      </Grid.Col>
-    </Grid>
+              <Indicator
+                size={16}
+                label={totalQuantity}
+                disabled={totalQuantity === 0}
+                color="cyan.9"
+              >
+                <ShoppingCart color="white" />
+              </Indicator>
+            </ActionIcon>
+          </Group>
+        </Grid.Col>
+      </Grid>
+    </MediaQuery>
   );
 };
 
