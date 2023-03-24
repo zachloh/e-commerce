f26fb2d974076906408c6f4bfc65ad79ff0c37c2
zachloh <leongaun17@gmail.com>
2023-03-24 18:13:19 +1100

    Make ProductsFilter sticky

--- src/pages/category/[category].tsx
+++ src/pages/category/[category].tsx
@@ -8,7 +8,7 @@ import { useFilter } from '@/hooks/useFilter';
 import { Product } from '@/types';
 import { filterProducts } from '@/utils/filterProducts';
 import { sortProducts } from '@/utils/sortProducts';
-import { Container, Divider, Grid, MediaQuery } from '@mantine/core';
+import { Container, Grid, MediaQuery, rem } from '@mantine/core';
 import { useDebouncedValue } from '@mantine/hooks';
 import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from 'next';
 import { ParsedUrlQuery } from 'querystring';
@@ -71,55 +71,83 @@ const Category = ({
   const [debouncedSortBy] = useDebouncedValue(sortBy, 300);
   const { filter, debouncedFilter, setFilter, resetFilter } = useFilter();
 
+  // TODO: reset state on route change
+
   // TODO: Add useMemo
   const filteredProducts = filterProducts(products, debouncedFilter);
   const sortedProducts = sortProducts(filteredProducts, debouncedSortBy);
 
-  // TODO: Change to sticky behavior
-
   return (
-    <Container size={1400} p={16}>
-      <CategoryHeader
-        category={category}
-        sortedProducts={sortedProducts}
-        sortBy={sortBy}
-        setSortBy={setSortBy}
-      />
-      <Divider my={10} />
-      <Grid m={0} gutterXs={32} columns={24}>
-        <Grid.Col p={0} span={24} xs={12} sm={9} md={8} lg={7}>
-          <FilterAccordion
-            sortedProducts={sortedProducts}
-            resetFilter={resetFilter}
+    <MediaQuery
+      largerThan={768}
+      styles={{ paddingLeft: rem(32), paddingRight: rem(32) }}
+    >
+      <Container size={1400} px={16} pb={30}>
+        <CategoryHeader
+          category={category}
+          sortedProducts={sortedProducts}
+          sortBy={sortBy}
+          setSortBy={setSortBy}
+        />
+        <Grid m={0} gutterXs={32} columns={24} align="flex-start">
+          <Grid.Col
+            px={0}
+            pt={10}
+            mb={12}
+            bg="white"
+            span={24}
+            xs={12}
+            sm={9}
+            md={8}
+            lg={7}
+            sx={{
+              position: 'sticky',
+              top: '180px',
+              zIndex: 12,
+              '@media (max-width: 575px)': {
+                top: '70px',
+              },
+            }}
           >
-            <ProductsFilter
-              filter={filter}
-              setFilter={setFilter}
+            <FilterAccordion
+              sortedProducts={sortedProducts}
               resetFilter={resetFilter}
-              sortBy={sortBy}
-              setSortBy={setSortBy}
-            />
-          </FilterAccordion>
-          <MediaQuery smallerThan="xs" styles={{ display: 'none' }}>
-            <div>
+            >
               <ProductsFilter
                 filter={filter}
                 setFilter={setFilter}
                 resetFilter={resetFilter}
                 sortBy={sortBy}
                 setSortBy={setSortBy}
+                height={228}
+                category={category}
+                id={1}
               />
-            </div>
-          </MediaQuery>
-        </Grid.Col>
-        <Grid.Col p={0} span={24} xs={12} sm={15} md={16} lg={17}>
-          <ProductsGrid
-            products={sortedProducts}
-            recommendedProducts={recommendedProducts}
-          />
-        </Grid.Col>
-      </Grid>
-    </Container>
+            </FilterAccordion>
+            <MediaQuery smallerThan="xs" styles={{ display: 'none' }}>
+              <div>
+                <ProductsFilter
+                  filter={filter}
+                  setFilter={setFilter}
+                  resetFilter={resetFilter}
+                  sortBy={sortBy}
+                  setSortBy={setSortBy}
+                  height="100%"
+                  category={category}
+                  id={2}
+                />
+              </div>
+            </MediaQuery>
+          </Grid.Col>
+          <Grid.Col p={0} span={24} xs={12} sm={15} md={16} lg={17}>
+            <ProductsGrid
+              products={sortedProducts}
+              recommendedProducts={recommendedProducts}
+            />
+          </Grid.Col>
+        </Grid>
+      </Container>
+    </MediaQuery>
   );
 };
 
