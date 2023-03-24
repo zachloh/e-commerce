454e9e33bedf7e249c9053c90a4df58dd2efe7c7
zachloh <leongaun17@gmail.com>
2023-03-24 16:13:39 +1100

    Change clothing types for men and women

--- src/components/ProductsFilter/TypeFilter/index.tsx
+++ src/components/ProductsFilter/TypeFilter/index.tsx
@@ -4,9 +4,10 @@ import React from 'react';
 type TypeFilterProps = {
   types: string[];
   setTypes: React.Dispatch<React.SetStateAction<string[]>>;
+  category: string;
 };
 
-const TypeFilter = ({ types, setTypes }: TypeFilterProps) => {
+const TypeFilter = ({ types, setTypes, category }: TypeFilterProps) => {
   return (
     <Accordion.Item value="type">
       <Accordion.Control>Type</Accordion.Control>
@@ -14,9 +15,20 @@ const TypeFilter = ({ types, setTypes }: TypeFilterProps) => {
         <Checkbox.Group value={types} onChange={setTypes}>
           <Stack spacing={10}>
             <Checkbox value="Shirts" label="Shirts" />
-            <Checkbox value="Suits" label="Suits" />
-            <Checkbox value="Hoodies" label="Hoodies" />
-            <Checkbox value="T-Shirts" label="T-Shirts" />
+            {category === 'men' && (
+              <>
+                <Checkbox value="Suits" label="Suits" />
+                <Checkbox value="Hoodies" label="Hoodies" />
+                <Checkbox value="T-Shirts" label="T-Shirts" />
+              </>
+            )}
+            {category === 'women' && (
+              <>
+                <Checkbox value="Tops" label="Tops" />
+                <Checkbox value="Jackets" label="Jackets" />
+                <Checkbox value="Dresses" label="Dresses" />
+              </>
+            )}
           </Stack>
         </Checkbox.Group>
       </Accordion.Panel>
