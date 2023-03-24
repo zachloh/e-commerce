faef9086eeea3909597b393142a4a8592883828c
zachloh <leongaun17@gmail.com>
2023-03-24 18:07:51 +1100

    Refactor styles for ProductsGrid

--- src/components/ProductsGrid/index.tsx
+++ src/components/ProductsGrid/index.tsx
@@ -13,16 +13,16 @@ const ProductsGrid = ({ products, recommendedProducts }: ProductsGridProps) => {
   if (products.length === 0) {
     return (
       <Box w="100%">
-        <Title order={2} pt={28} size={20} weight={600}>
+        <Title order={2} size={20} weight={600} className={styles.title}>
           We couldn&apos;t find any results
         </Title>
-        <Text mt={20} color="dark.3" size={15}>
+        <Text mt={10} color="dark.3" size={15}>
           Please adjust or clear filters to view more products.
         </Text>
-        <Title mt={50} mb={-20} order={3} size={18} weight={600}>
+        <Title mt={30} mb={5} order={3} size={18} weight={600}>
           Today&apos;s most popular
         </Title>
-        <div className={styles.grid}>
+        <div className={styles.recommended}>
           {recommendedProducts.map((product) => (
             <div key={product.id}>
               <ProductCard product={product} withHeart={false} />
