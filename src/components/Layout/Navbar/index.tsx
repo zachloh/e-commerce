700e764a12ae506b38d0e3ec1edba2b4a0d04429
zachloh <leongaun17@gmail.com>
2023-03-24 15:31:11 +1100

    Refactor NavLinks in Navbar

--- src/components/Layout/Navbar/index.tsx
+++ src/components/Layout/Navbar/index.tsx
@@ -1,12 +1,38 @@
 import { NavLink } from '@mantine/core';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 import React from 'react';
+import { RemoveScroll } from 'react-remove-scroll';
 
-const Navbar = () => {
+type NavbarProps = {
+  onClose: () => void;
+};
+
+const Navbar = ({ onClose }: NavbarProps) => {
+  const router = useRouter();
+  const currentRoute = router.asPath;
+
   return (
     <>
-      <NavLink label="Men" />
-      <NavLink label="Women" />
-      <NavLink label="Kids" />
+      <RemoveScroll>
+        <NavLink
+          label="Men"
+          component={Link}
+          href="/category/men"
+          onClick={onClose}
+          active={currentRoute === '/category/men'}
+          color="cyan.9"
+          mb={5}
+        />
+        <NavLink
+          label="Women"
+          component={Link}
+          href="/category/women"
+          onClick={onClose}
+          active={currentRoute === '/category/women'}
+          color="cyan.9"
+        />
+      </RemoveScroll>
     </>
   );
 };
