2e8bf2b96678b65842217fbf224adc3644351d2d
zachloh <leongaun17@gmail.com>
2023-03-26 20:12:33 +1100

    Create an axios instance for checkout requests

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -6,3 +6,10 @@ export const customAxios = axios.create({
     Authorization: `Bearer ${process.env.API_TOKEN}`,
   },
 });
+
+export const stripeAxios = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  headers: {
+    Authorization: `Bearer ${process.env.NEXT_PUBLIC_ORDER_TOKEN}`,
+  },
+});
