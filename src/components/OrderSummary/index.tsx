bf4105614ece4cdf777c4da394f2a62afdba1a9c
zachloh <leongaun17@gmail.com>
2023-03-26 20:42:50 +1100

    Handle checkout with stripe prebuilt checkout page

--- src/components/OrderSummary/index.tsx
+++ src/components/OrderSummary/index.tsx
@@ -1,15 +1,53 @@
-import { useTotalPrice } from '@/stores/cart';
 import { Button, Card, Divider, Group, Text } from '@mantine/core';
-import React from 'react';
+import { notifications } from '@mantine/notifications';
+import React, { useState } from 'react';
+import { ExclamationMark } from 'tabler-icons-react';
+import { stripeAxios } from '@/lib/axios';
+import { useCart, useTotalPrice } from '@/stores/cart';
+import getStripe from '@/utils/getStripe';
 import styles from './OrderSummary.module.css';
 
 const OrderSummary = () => {
   const { totalPrice, isHydrated } = useTotalPrice();
+  const { cart } = useCart();
+  const [loading, setLoading] = useState(false);
 
-  // TODO: Show skeleton
   if (!isHydrated) {
     return null;
   }
+
+  const handleCheckout = async () => {
+    try {
+      setLoading(true);
+
+      const response = await stripeAxios.post('/orders', {
+        products: cart.map((item) => ({
+          id: item.id,
+          quantity: item.quantity,
+          size: item.size,
+        })),
+      });
+
+      const stripe = await getStripe();
+      const { error } = await stripe!.redirectToCheckout({
+        sessionId: response.data.stripeSession.id,
+      });
+
+      setLoading(false);
+
+      if (error) {
+        throw new Error(error.message);
+      }
+    } catch (err) {
+      notifications.show({
+        title: 'Unexpected Error',
+        message: 'Please try again later.',
+        color: 'red',
+        icon: <ExclamationMark />,
+      });
+      setLoading(false);
+    }
+  };
 
   return (
     <Card
@@ -35,7 +73,15 @@ const OrderSummary = () => {
         <Text weight={600}>Total</Text>
         <Text weight={600}>${totalPrice}.00</Text>
       </Group>
-      <Button w="100%" h={50} size="md" radius={0} mt={20}>
+      <Button
+        w="100%"
+        h={50}
+        size="md"
+        radius={0}
+        mt={20}
+        onClick={handleCheckout}
+        loading={loading}
+      >
         CHECKOUT
       </Button>
     </Card>
